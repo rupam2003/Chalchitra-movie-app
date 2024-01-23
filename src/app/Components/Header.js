086ade5e5631506848fdc6c922f8f829517aa496
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-01-23 19:00:41 +0530

    Update Header.js logo added

--- src/app/Components/Header.js
+++ src/app/Components/Header.js
@@ -2,6 +2,7 @@
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import React from 'react'
+import Image from 'next/image'
 import { PiFilmReel } from "react-icons/pi";
 
 const Header = () => {
@@ -17,8 +18,9 @@ const Header = () => {
     <header className='bg-black'>
         <section className=' flex py-2 justify-between'>
         <Link href={'/'}  className='flex font-bold  ml-2 text-2xl'>
-          <PiFilmReel className='text-3xl mx-1'/>
-          CHALCHITRA
+          <div>
+            <Image quality={100} src="/images.png" width={80} height={50}/>
+          </div>
           </Link>
           <form className='mx-2' onSubmit={handleSubmit}> 
             <input id='input' autoComplete='off' className='bg-blue-100 bg-opacity-10 pl-3 py-2 min-w-[15rem] text-xs  text-white outline-none rounded-full' placeholder='Search any movie...'/>
